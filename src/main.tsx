cbb41543c186195d38403f982052ca3bb7f4365d
Shynn <shynn@me.com>
2025-03-20 09:56:06 +0000

    test: verify import sorting rules

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,10 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './index.css';
-import App from './App';
 import { ThemeProvider } from '@context/theme/ThemeProvider';
+
+import App from './App';
+
+import './index.css';
 
 const rootElement = document.getElementById('root');
 
