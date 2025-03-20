45ac5ffa99bd6e38a271bd975410cff4c470b5f4
Shynn <shynn@me.com>
2025-03-20 09:55:40 +0000

    chore: update eslint and lint-staged configuration

--- eslint.config.js
+++ eslint.config.js
@@ -4,6 +4,7 @@ import tsPlugin from '@typescript-eslint/eslint-plugin';
 import reactPlugin from 'eslint-plugin-react';
 import reactHooksPlugin from 'eslint-plugin-react-hooks';
 import reactRefreshPlugin from 'eslint-plugin-react-refresh';
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
 
 export default [
 	{
@@ -28,6 +29,7 @@ export default [
 			react: reactPlugin,
 			'react-hooks': reactHooksPlugin,
 			'react-refresh': reactRefreshPlugin,
+			'simple-import-sort': simpleImportSort,
 		},
 		rules: {
 			...tsPlugin.configs.recommended.rules,
@@ -38,6 +40,34 @@ export default [
 			'react-hooks/rules-of-hooks': 'error',
 			'react-hooks/exhaustive-deps': 'warn',
 			'react/react-in-jsx-scope': 'off',
+			'simple-import-sort/imports': [
+				'error',
+				{
+					groups: [
+						// React and external packages
+						['^react', '^@?\\w'],
+						// Internal imports with alias (@)
+						['^@\\w'],
+						// Relative imports
+						['^\\./(?=.*/)(?!/?$)', '^\\.(?!/?$)', '^\\./?$'],
+						// Style imports
+						['^.+\\.css$'],
+					],
+				},
+			],
+			'simple-import-sort/exports': 'error',
+			'padding-line-between-statements': [
+				'error',
+				{ blankLine: 'always', prev: 'import', next: '*' },
+				{ blankLine: 'any', prev: 'import', next: 'import' },
+			],
+			'react/jsx-uses-react': 'off',
+			'react/prop-types': 'off',
+			'@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+			'@typescript-eslint/no-explicit-any': 'warn',
+			'@typescript-eslint/explicit-function-return-type': 'off',
+			'@typescript-eslint/explicit-module-boundary-types': 'off',
+			'@typescript-eslint/no-non-null-assertion': 'warn',
 		},
 		settings: {
 			react: {
