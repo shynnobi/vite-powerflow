f5a49f769f8caec65d986e63a12e147c925638de
Shynn <shynn@me.com>
2025-03-20 10:31:11 +0000

    test: fix decrement test case

---
+++ src/pages/Blog.tsx
@@ -0,0 +1,9 @@
+import { type ReactElement } from 'react';
+
+export default function Blog(): ReactElement {
+	return (
+		<div className="flex min-h-screen items-center justify-center bg-background">
+			<h1 className="text-4xl font-bold text-foreground">Blog Page</h1>
+		</div>
+	);
+}
