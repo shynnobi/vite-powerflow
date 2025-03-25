237a557a3bdeb1ae5e26121c178d4658df9287ad
Shynn <shynn@me.com>
2025-03-25 18:06:51 +0100

    fix: disable react-refresh/only-export-components rule for UI components

--- eslint.config.js
+++ eslint.config.js
@@ -76,4 +76,11 @@ export default [
 			},
 		},
 	},
+	// Specific rule for UI components
+	{
+		files: ['**/src/components/ui/**/*.{ts,tsx}'],
+		rules: {
+			'react-refresh/only-export-components': 'off',
+		},
+	},
 ];
