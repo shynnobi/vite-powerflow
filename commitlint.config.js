c988d1c22471d2e2d9d0f359124476b66665db6a
Shynn <shynn@me.com>
2025-05-23 13:12:07 +0000

    chore: update commitlint config for stricter enforcement

--- commitlint.config.js
+++ commitlint.config.js
@@ -5,7 +5,7 @@ export default {
 		'body-max-line-length': [2, 'always', 100],
 		'footer-leading-blank': [1, 'always'],
 		'footer-max-line-length': [2, 'always', 100],
-		'header-max-length': [2, 'always', 100],
+		'header-max-length': [2, 'always', 72],
 		'subject-case': [2, 'never', ['sentence-case', 'start-case', 'pascal-case', 'upper-case']],
 		'subject-empty': [2, 'never'],
 		'subject-full-stop': [2, 'never', '.'],
